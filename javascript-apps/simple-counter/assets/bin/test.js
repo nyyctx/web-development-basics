264e02f11fe97ba4939b5612c31ca6b88bfe4058
nnlucas <lucas_nunes_ribeiro@hotmail.com>
2022-06-17 23:03:35 -0300

    Atualização com JavaScript

---
+++ javascript-apps/simple-counter/assets/bin/test.js
@@ -0,0 +1,16 @@
+//funções, variáveis e constantes
+
+function soma(x, y) {
+    var x;
+    var y;
+    return x + y;
+}
+
+soma (4, 6);
+
+//console replit.com/node
+
+console.log("Hello World!");    
+
+//
+
